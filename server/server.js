334146ea312b8ba26812c2f9c81550946c7af0b7
EnADEL9 <adilalrys7@gmail.com>
2026-06-25 22:06:47 +0300

    exporting the server app.

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from 'inngest/express'
-import { inngest, functions } from './inngest/index'
+import { inngest, functions } from './inngest/index.js'
 
 const app = express()
 
@@ -17,4 +17,8 @@ app.use("/api/inngest", serve({ client: inngest, functions }))
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
+}
+
+export default app
