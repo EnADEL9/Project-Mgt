f10a8a48f281b14f5660b350e7d4dc985835d0ea
EnADEL9 <adilalrys7@gmail.com>
2026-06-25 22:22:03 +0300

    inngest edited.

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -5,14 +5,13 @@ export const inngest = new Inngest({ id: "project-management" });
 
 // A function to save user to the database
 const syncUserCreation = inngest.createFunction(
-    { id: 'sync-user-from-clerk' },
-    { event: 'clerk/user.created' },
-    async ({event}) => {
-        const {data} = event
+    { id: 'sync-user-from-clerk', triggers: { event: 'clerk/user.created' } },
+    async ({ event }) => {
+        const { data } = event
         await prisma.user.create({
             data: {
                 id: data.id,
-                email: data?.email_addresses[0]?.email_addres,
+                email: data?.email_addresses[0]?.email_address,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url
             }
@@ -22,31 +21,28 @@ const syncUserCreation = inngest.createFunction(
 
 // A function to delete user from database
 const syncUserDeletion = inngest.createFunction(
-    { id: 'delete-user-with-clerk' },
-    { event: 'clerk/user.deleted' },
-    async ({event}) => {
-        const {data} = event
+    { id: 'delete-user-with-clerk', triggers: { event: 'clerk/user.deleted' } },
+    async ({ event }) => {
+        const { data } = event
         await prisma.user.delete({
             where: {
-            id: data.id,
+                id: data.id,
             }
         })
     }
 )
 
-
 // A function to update user in database
 const syncUserUpdation = inngest.createFunction(
-    { id: 'update-user-from-clerk' },
-    { event: 'clerk/user.updated' },
-    async ({event}) => {
-        const {data} = event
+    { id: 'update-user-from-clerk', triggers: { event: 'clerk/user.updated' } },
+    async ({ event }) => {
+        const { data } = event
         await prisma.user.update({
             where: {
                 id: data.id,
             },
             data: {
-                email: data?.email_addresses[0]?.email_addres,
+                email: data?.email_addresses[0]?.email_address,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url
             }
